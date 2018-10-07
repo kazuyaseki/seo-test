cc5cd3ff29341da560b20803ec516ca28b6194c2
kazuyaseki <track613armory@gmail.com>
2018-10-07 20:44:43 +0900

    Play

--- basis/src/index.js
+++ basis/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const title = 'Hello Netlify';
 
-ReactDOM.render(<div>{title}</div>, document.getElementById('app'));
+class App extends React.Component {
+  componentDidMount() {
+    fetch(
+      'https://fervent-torvalds-4cb1f2.netlify.com/.netlify/functions/hello'
+    ).then(res => res.json());
+  }
+
+  render() {
+    return <div>{title}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
